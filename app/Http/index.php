10b4dc91091b3616e7262960fab9eebe4c917eb7
Bushra <77388684+bushra1997@users.noreply.github.com>
2022-02-14 18:49:10 +0200

    files

---
+++ app/Http/index.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+if (isset($_SESSION['user']) && isset($_SESSION['password'])) {
+    header('Location: /test5/home.php' . '?users' . '/' . $_SESSION['user']);
+} else {
+    header('Location: /test5/form.php');
+}
